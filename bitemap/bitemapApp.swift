c7cf63b97aadb89b3e54593bde39291abf44b458
Simonas Kytra <simonaskytra@gmail.com>
2023-08-29 13:39:30 +0300

    Initial Commit

---
+++ bitemap/bitemapApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitemapApp.swift
+//  bitemap
+//
+//  Created by Simonas Kytra on 2023-08-29.
+//
+
+import SwiftUI
+
+@main
+struct bitemapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
